86db13684b5aec5f15512c76860f241d6806dc21
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-15 08:26:01 -0300

    Trabalhando com funções

--- src/components/AllBackground/index.tsx
+++ src/components/AllBackground/index.tsx
@@ -1,8 +1,16 @@
+import { FormEvent } from 'react';
 import { Form } from '../Form';
 
 import style from './style.module.scss';
 
 export function AllBackground() {
+  
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    alert("Testando");
+  }
+
   return(
     <div className={style.allBackground}>
       <div className={style.contentAllBackground}>
@@ -14,7 +22,7 @@ export function AllBackground() {
             Access them wherever you need, share and collaborate with friends,
              family, and co-workers.
           </p>
-          <Form placeholder="Enter you email..." />
+          <Form onSubmit={handleSubmit} placeholder="Enter you email..." />
         </article>
         <img src="/images/illustration-1.svg" alt="Ilustration-1" />
       </div>
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,7 +2,7 @@ import style from './style.module.scss';
 
 export function Form(props) {
   return(
-    <form action="" className={style.form}>
+    <form onSubmit={props.onSubmit} className={style.form}>
       <input type="text" placeholder={props.placeholder} />
       <button>Get Started</button>
     </form>
