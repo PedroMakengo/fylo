7c7ac4a05a53c9c623de2c8bc142bd539812020a
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-15 05:31:51 -0300

    Melhorando o componente AllBackground

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default class MyDocuments extends Document {
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
       </Head>
     <body>
       <Main />
