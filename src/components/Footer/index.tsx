5e9a5d75204be2b781dfeb93de9a2fddde24205b
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-19 07:29:13 -0300

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,35 @@ export function Footer() {
   return (
     <footer className={style.footer}>
       <div className={style.contentFooter}>
-        <h1>Footer</h1>
+        <div className={style.item}>
+          <img src="/images/logo-footer.svg" alt="" />
+          <ul>
+            <li>Phone: +1-543-123-4567</li>
+            <li>example@fylo.com</li>
+          </ul>
+        </div>
+        <div className={style.item}>
+          <nav>
+            <a href="#">About Us</a>
+            <a href="#">Jobs</a>
+            <a href="#">Press</a>
+            <a href="#">Blog</a>
+          </nav>
+        </div>
+        <div className={style.item}>
+          <nav>
+            <a href="#">Contact Us</a>
+            <a href="#">Terms</a>
+            <a href="#">Privacy</a>
+          </nav>
+        </div>
+        <div className={style.item}>
+          <nav>
+            <a href="#">Contact Us</a>
+            <a href="#">Terms</a>
+            <a href="#">Privacy</a>
+          </nav>
+        </div>
       </div>
     </footer>
   );
