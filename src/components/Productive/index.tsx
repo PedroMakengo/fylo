015eaa7f30c043447a12818b65f2d34dd80d0bed
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-19 06:42:49 -0300

    Componente AllBackground

--- src/components/Productive/index.tsx
+++ src/components/Productive/index.tsx
@@ -1,34 +1,42 @@
-import { FaArrowAltCircleRight } from 'react-icons/fa'
-import style from './style.module.scss';
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import style from "./style.module.scss";
 
 export function Productive() {
   return (
-    <section>
+    <section className={style.productive}>
       <div className={style.background}></div>
       <div className={style.contentProductive}>
-        <article>
-          <h1>Stay productive, wherever you are</h1>
-          <p>Never let location be an issue when accessing your files. Fylo 
-            has you covered for all of your file storage needs.</p>
-
-          <p>Securely share files and folders with friends, family and 
-            colleagues for live collaboration. No email attachments required!
-          </p>
-
-          <a href="#">See how Fylo works <FaArrowAltCircleRight /></a>
-
-          <div>
-            <p>Fylo has improved our team productivity by an order of 
-              magnitude. Since making the switch our team has become a
-              well-oiled collaboration machine.
+        <div className={style.container}>
+          <article>
+            <h1>Stay productive, wherever you are</h1>
+            <p>
+              Never let location be an issue when accessing your files. Fylo has
+              you covered for all of your file storage needs.
             </p>
+
+            <p>
+              Securely share files and folders with friends, family and
+              colleagues for live collaboration. No email attachments required!
+            </p>
+
+            <a href="#">
+              See how Fylo works <FaArrowAltCircleRight />
+            </a>
+
             <div>
-              <img src="./images/avatar-testimonial.jpg" alt="" />
+              <p>
+                Fylo has improved our team productivity by an order of
+                magnitude. Since making the switch our team has become a
+                well-oiled collaboration machine.
+              </p>
+              <div>
+                <img src="./images/avatar-testimonial.jpg" alt="" />
+              </div>
             </div>
-          </div>
-        </article>
-        <img src="/images/illustration-2.svg" alt="" />
+          </article>
+          <img src="/images/illustration-2.svg" alt="" />
+        </div>
       </div>
     </section>
-  )
+  );
 }
